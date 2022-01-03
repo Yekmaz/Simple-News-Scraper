da9059e7c86a7489cb55c186fba33d991f72e6d8
Yekmaz <m.yekmaz@gmail.com>
2022-01-03 09:49:38 +0330

    add exception handler and view

--- news_scraper/views.py
+++ news_scraper/views.py
@@ -3,33 +3,35 @@ import requests
 from bs4 import BeautifulSoup
 
 def news_list(request):
-    page = requests.get("https://www.irna.ir/archive?pl=2300")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    
-    news_titles=soup.select("#box4 h3 a")
-    titles = [nt.get_text() for nt in news_titles]
-
-    img_list = soup.select("#box4 img")
-    imgs = [il.get('src') for il in img_list]
-
-    news_leads=soup.select(".desc p")
-    leads = [nl.get_text() for nl in news_leads]
-    
-    for i in range(30):
-        str=leads[i]
-        x= str.rfind('-')
-        leads[i]= str[x+1:]
-
-    news_times=soup.select("time a")
-    times = [nt.get_text() for nt in news_times]
-
-    news = [['' for i in range(4)] for j in range(30)]
-    for i in range(30):
-        news[i][0]=titles[i]
-        news[i][1]=imgs[i]
-        news[i][2]=leads[i]
-        news[i][3]=times[i]
+    try:
+        page = requests.get("https://www.irna.ir/archive?pl=2300")
+        soup = BeautifulSoup(page.content, 'html.parser')
         
+        news_titles=soup.select("#box4 h3 a")
+        titles = [nt.get_text() for nt in news_titles]
 
+        img_list = soup.select("#box4 img")
+        imgs = [il.get('src') for il in img_list]
 
-    return render(request, 'news_scraper/news_list.html', {'news':news})
+        news_leads=soup.select(".desc p")
+        leads = [nl.get_text() for nl in news_leads]
+        
+        for i in range(30):
+            str=leads[i]
+            x= str.rfind('-')
+            leads[i]= str[x+1:]
+
+        news_times=soup.select("time a")
+        times = [nt.get_text() for nt in news_times]
+
+        news = [['' for i in range(4)] for j in range(30)]
+        for i in range(30):
+            news[i][0]=titles[i]
+            news[i][1]=imgs[i]
+            news[i][2]=leads[i]
+            news[i][3]=times[i]
+            
+        return render(request, 'news_scraper/news_list.html', {'news':news})
+
+    except:
+        return render(request, 'news_scraper/error.html', {})
