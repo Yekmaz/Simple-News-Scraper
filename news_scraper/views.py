aace52a9f4ef840cc6f658024f7c79d33146c00b
Yekmaz <m.yekmaz@gmail.com>
2022-01-03 08:37:06 +0330

    scrap news and show them

--- news_scraper/views.py
+++ news_scraper/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+def news_list(request):
+    page = requests.get("https://www.irna.ir/archive?pl=2300")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    news_titles=soup.select("#box4 h3 a")
+    titles = [nt.get_text() for nt in news_titles]
+
+    img_list = soup.select("#box4 img")
+    imgs = [il.get('src') for il in img_list]
+
+    news_leads=soup.select(".desc p")
+    leads = [nl.get_text() for nl in news_leads]
+    
+    for i in range(30):
+        str=leads[i]
+        x= str.rfind('-')
+        leads[i]= str[x+1:]
+
+    news_times=soup.select("time a")
+    times = [nt.get_text() for nt in news_times]
+
+    news = [['' for i in range(4)] for j in range(30)]
+    for i in range(30):
+        news[i][0]=titles[i]
+        news[i][1]=imgs[i]
+        news[i][2]=leads[i]
+        news[i][3]=times[i]
+        
+
+
+    return render(request, 'news_scraper/news_list.html', {'news':news})
